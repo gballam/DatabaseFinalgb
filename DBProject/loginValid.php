9edd5113076b07b91f982e1f0984992967bb7f32
gballam <31631248+gballam@users.noreply.github.com>
2017-11-28 21:26:26 -0600

    Fixed users table name
    replaced all instances of 'raffleMain' with 'users_table' (the renamed users table)

--- DBProject/loginValid.php
+++ DBProject/loginValid.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 echo "Connected successfully \n";
 $username = $_SESSION["user"];
 $password = $_SESSION["pwd"];
-$query = $conn->prepare("SELECT password from raffleMain where username=?");
+$query = $conn->prepare("SELECT password from users_table where username=?");
   $query->bind_param("s", $username);
    $query->execute();
    $query->store_result();
--- DBProject/registerValid.php
+++ DBProject/registerValid.php
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
 echo "Connected successfully \n";
 $username = $_SESSION["user"];
 $password = $_SESSION["pwd"];
-$query = $conn->prepare("SELECT password from raffleMain where username=?");
+$query = $conn->prepare("SELECT password from users_table where username=?");
   $query->bind_param("s", $username);
    $query->execute();
    $query->store_result();
@@ -31,7 +31,7 @@ $query = $conn->prepare("SELECT password from raffleMain where username=?");
    }
    else
    {
-       $sql = "INSERT INTO raffleMain (username, password, ticketNums)
+       $sql = "INSERT INTO users_table (username, password, ticketNums)
        VALUES ('".$_SESSION["user"]."', '".$_SESSION["pwd"]."', '0')";
        if ($conn->query($sql) === TRUE) {
              $_SESSION["loggedIn"] = TRUE;
